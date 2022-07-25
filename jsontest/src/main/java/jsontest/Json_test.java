5a04a264667c5099766865735b28073cdc1f0492
nikhilm8147 <88988266+nikhilm8147@users.noreply.github.com>
2022-07-25 14:58:14 +0530

    Added test functionality to validate team in Json_test.java file

---
+++ jsontest/src/main/java/jsontest/Json_test.java
@@ -0,0 +1,117 @@
+package jsontest;
+
+import java.util.List;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+
+public class Json_test {
+
+	public static void main(String[] args) {
+		
+		String json = "{\r\n"
+				+ "  \"name\": \"Royal Challengers Bangalore\",\r\n"
+				+ "  \"location\": \"Bangalore\",\r\n"
+				+ "  \"player\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Faf Du Plessis\",\r\n"
+				+ "      \"country\": \"South Africa\",\r\n"
+				+ "      \"role\": \"Batsman\",\r\n"
+				+ "      \"price-in-crores\": \"7\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Virat Kohli\",\r\n"
+				+ "      \"country\": \"India\",\r\n"
+				+ "      \"role\": \"Batsman\",\r\n"
+				+ "      \"price-in-crores\": \"15\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Glenn Maxwell\",\r\n"
+				+ "      \"country\": \"Australia\",\r\n"
+				+ "      \"role\": \"Batsman\",\r\n"
+				+ "      \"price-in-crores\": \"11\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Mohammad Siraj\",\r\n"
+				+ "      \"country\": \"India\",\r\n"
+				+ "      \"role\": \"Bowler\",\r\n"
+				+ "      \"price-in-crores\": \"7\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Harshal Patel\",\r\n"
+				+ "      \"country\": \"India\",\r\n"
+				+ "      \"role\": \"Bowler\",\r\n"
+				+ "      \"price-in-crores\": \"10.75\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Wanindu Hasaranga\",\r\n"
+				+ "      \"country\": \"Srilanka\",\r\n"
+				+ "      \"role\": \"Bowler\",\r\n"
+				+ "      \"price-in-crores\": \"10.75\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Dinesh Karthik\",\r\n"
+				+ "      \"country\": \"India\",\r\n"
+				+ "      \"role\": \"Wicket-keeper\",\r\n"
+				+ "      \"price-in-crores\": \"5.5\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Shahbaz Ahmed\",\r\n"
+				+ "      \"country\": \"India\",\r\n"
+				+ "      \"role\": \"All-Rounder\",\r\n"
+				+ "      \"price-in-crores\": \"2.4\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Rajat Patidar\",\r\n"
+				+ "      \"country\": \"India\",\r\n"
+				+ "      \"role\": \"Batsman\",\r\n"
+				+ "      \"price-in-crores\": \"0.20\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Josh Hazlewood\",\r\n"
+				+ "      \"country\": \"Australia\",\r\n"
+				+ "      \"role\": \"Bowler\",\r\n"
+				+ "      \"price-in-crores\": \"7.75\"\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"name\": \"Mahipal Lomror\",\r\n"
+				+ "      \"country\": \"India\",\r\n"
+				+ "      \"role\": \"Bowler\",\r\n"
+				+ "      \"price-in-crores\": \"7.75\"\r\n"
+				+ "    }\r\n"
+				+ "  ]\r\n"
+				+ "}";
+		
+		Configuration conf = Configuration.defaultConfiguration();
+		Configuration conff = conf.addOptions(Option.ALWAYS_RETURN_LIST);
+		
+		Boolean flag=true;
+
+		//Works fine
+		List<String> foreign_players = JsonPath.using(conff).parse(json).read("$.player[?(@.country!='India')].name");
+
+		List<String> wicket_keepers = JsonPath.using(conff).parse(json).read("$.player[?(@.role=='Wicket-keeper')].name");
+
+		
+		
+		if(foreign_players.size()!=4) {
+			System.out.println("Team is having "+foreign_players.size()+" players");
+			System.err.println("Team should have only 4 foreign players");
+			flag=false;
+		}
+		
+		if(wicket_keepers.size()<1) {
+			System.out.println("Team should have atleast 1 wicket keeper");
+			System.err.println("Wicket count is "+wicket_keepers.size());
+			flag=false;
+		}
+		
+		if(flag==true) {
+			System.out.println("Team is good to go");
+			
+		}
+		
+	}
+
+}
